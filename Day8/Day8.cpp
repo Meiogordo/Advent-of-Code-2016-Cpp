6aa78884234b073d80d175e08a05f5c13bdcb189
Meiogordo <gamer-go@hotmail.com>
2017-07-25 12:41:57 +0100

    Started Day8, only templates, because it seems too hard and I have no time atm

---
+++ Day8/Day8.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "Utils/Utilities.h"
+
+using namespace std;
+
+
+//Protos
+
+
+
+int main() {
+	
+	vector<string> text = Utilities::ReadFile("Day4/Input.txt");
+	//vector<string> text = {""}; //Manual for debugging
+	
+	return 0;
+}
